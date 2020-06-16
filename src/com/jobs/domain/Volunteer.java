91b16423a2aa4fd987f538b52bee67c16dcaf5ab
cotoflux <guaupelu@hotmail.com>
2020-06-16 10:48:22 +0200

    Finished Volunter, we do not pay so nothing done in the pay now

--- src/com/jobs/domain/Volunteer.java
+++ src/com/jobs/domain/Volunteer.java
@@ -3,12 +3,26 @@ package com.jobs.domain;
 public class Volunteer extends AbsStaffMember {
 	
 
+	private String description;
+
+
 	public Volunteer(String name, String address, String phone, String description) throws Exception {
 		super(name, address, phone);
+		//created the variable is not herited
+		this.description = description;
 		
-		//TODO
 	}
 
+	//Created the toString
+	@Override
+	public String toString() {
+		
+		return "\nVolunteer \n name : " + name +
+				"\n adress : " +address+
+				"\n phone : " + phone+
+				"\n description: " + this.description + ".";
+	}
+	
 	
 	@Override
 	public void pay() {
